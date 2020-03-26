d7fb26b96f9917290071e054c679e3c225c04ba2
qwainaina <accounts@qwainaina.com>
2020-03-26 19:48:41 +0300

    Updated to favour Quad-Cores and Below also streamlined timezone Rewards

--- optimizer.c
+++ optimizer.c
@@ -91,6 +91,7 @@ int get_time_zone_reward() {
 	int timezone = get_timezone();
 	if ((EARLIEST_AFRICAN_TIMEZONE < timezone) && (timezone <= EARLIEST_EUROPEAN_TIMEZONE)) {
 		return 30;
+
 	}
 	else
 	{
